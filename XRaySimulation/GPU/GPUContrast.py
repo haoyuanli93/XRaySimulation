c66a3698da908ebb71a55b7be5b268bdaff92a0b
Haoyuan Li <hyli16@stanford.edu>
2022-08-27 23:21:13 -0700

    Update GPUContrast.py

--- XRaySimulation/GPU/GPUContrast.py
+++ XRaySimulation/GPU/GPUContrast.py
@@ -117,6 +117,9 @@ def getContrastMethod1(eFieldComplexFiles, qVec, k0, nx, ny, nz, dx, dy, dz, nSa
             cohFunI = np.load(workDir + "cohFunI_{}.npy".format(batchIdx))
 
             contrast += np.square(np.abs(cohFunR)) + np.square(np.abs(cohFunI))
+        return contrast
+    else:
+        print("The coherence function has been save to {}".format(workDir))
 
 
 @cuda.jit('void(int64, int64, int64, int64,' +
@@ -153,6 +156,10 @@ def getCoherenceFunctionXY_GPU_Method1(nSpatial1,
     """
     idx1, idx2 = cuda.grid(2)
     if (idx1 < nSpatial1) & (idx2 < nSpatial2):
+        oldDeltaZ = 0
+        oldHolderRealTmp = 0.0
+        oldHolderImagTmp = 0.0
+
         deltaZxy = deltaZx[idx2] + deltaZy[idx2] - (
                 deltaZx[idx1] + deltaZy[idx1])  # get the contribution from the xy direction in Q*(r2-r1)/k0
 
@@ -160,6 +167,13 @@ def getCoherenceFunctionXY_GPU_Method1(nSpatial1,
             deltaZ = int(deltaZxy + deltaZz(sIdx))  # Get the contribution from the z dimension
 
             if abs(deltaZ) >= nz:
+                continue
+
+            # When deltaZ does not change, one does not need to calculate the time average again.
+            if deltaZ == oldDeltaZ:
+                # Because for the same z2 - z1, the summation is the same. One only needs to add a weight
+                holderReal[idx1, idx2] += oldHolderRealTmp * weight[sIdx]
+                holderImag[idx1, idx2] += oldHolderImagTmp * weight[sIdx]
                 continue
 
             # The delta Z determines the range over which time we calculate the average
@@ -182,11 +196,170 @@ def getCoherenceFunctionXY_GPU_Method1(nSpatial1,
             holderReal[idx1, idx2] += holderRealTmp * weight[sIdx]
             holderImag[idx1, idx2] += holderImagTmp * weight[sIdx]
 
+            # Update the infor for the same deltaZ
+            oldDeltaZ = int(deltaZ)
+            oldHolderRealTmp = float(holderRealTmp)
+            oldHolderImagTmp = float(holderImagTmp)
+
 
 #########################################################################################
 #                         Method 2
 #
 #            Get the contrast of each pulse individually.
+#########################################################################################
+def getContrastMethod2(eFieldComplexFiles, qVec, k0, nx, ny, nz, dx, dy, dz, nSampleZ, dSampleZ, ):
+    """
+    Very challenging calculation.
+    Need to check with Yanwen about the definition of the calculation.
+
+    :param eFieldComplexFiles:
+    :param qVec:
+    :param k0:
+    :param nx:
+    :param ny:
+    :param nz:
+    :param dx:
+    :param dy:
+    :param dz:
+    :param nSampleZ:
+    :param dSampleZ:
+    :return:
+    """
+    # Step1, prepare the variables
+    numXY = nx * ny
+
+    # The phase change according to Q/k0 * r
+    deltaZx = np.zeros((nx, ny))
+    deltaZx[:, :] = (np.arange(nx) * dx * qVec[0] / k0 / dz)[:, np.newaxis]
+    deltaZx = np.ascontiguousarray(np.reshape(deltaZx, nx * ny))
+
+    deltaZy = np.zeros((nx, ny))
+    deltaZy[:, :] = (np.arange(ny) * dy * qVec[1] / k0 / dz)[np.newaxis, :]
+    deltaZy = np.ascontiguousarray(np.reshape(deltaZy, nx * ny))
+
+    deltaZz = np.ascontiguousarray(np.arange(nz) * dz * qVec[2] / k0 / dz)
+
+    # Get the weight of the summation over z2-z1
+    weight = np.ascontiguousarray(dSampleZ - np.abs(np.arange(-(dSampleZ - 1), dSampleZ, 1)))
+
+    # Move the gpu to reduce traffic
+    cuDeltaZx = cuda.to_device(deltaZx)
+    cuDeltaZy = cuda.to_device(deltaZy)
+    cuDeltaZz = cuda.to_device(deltaZz)
+    cuWeight = cuda.to_device(weight)
+
+    # Step2, Loop through the electric field
+    contrastArray = np.zeros(len(eFieldComplexFiles), dtype=np.float64)
+
+    for eFieldIdx in range(len(eFieldComplexFiles)):
+        # Load the electric field
+        fileName = eFieldComplexFiles[eFieldIdx]
+        eFieldComplex = np.load(fileName)
+
+        eFieldRealFlat = np.ascontiguousarray(np.reshape(eFieldComplex.real, (nx * ny, nz)))
+        eFieldImagFlat = np.ascontiguousarray(np.reshape(eFieldComplex.imag, (nx * ny, nz)))
+        del eFieldComplex
+
+        # Define gpu calculation batch
+        threadsperblock = (16, 16)
+        blockspergrid_x = math.ceil(numXY / threadsperblock[0])
+        blockspergrid_y = math.ceil(numXY / threadsperblock[1])
+        blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+        # Update the coherence function
+        contrastArray[eFieldIdx] = getCoherenceFunctionXY_GPU_Method2[[blockspergrid, threadsperblock]](
+            numXY,
+            nz,
+            nSampleZ,
+            cuDeltaZx,
+            cuDeltaZy,
+            cuDeltaZz,
+            cuWeight,
+            eFieldRealFlat,
+            eFieldImagFlat,
+        ).copy_to_host()[0]
+
+    return contrastArray
+
+
+@cuda.jit('void(int64, int64, int64,' +
+          ' float64[:], float64[:], float64[:], float64[:], ' +
+          'float64[:,:], float64[:,:], float64[:,:], float64[:,:])')
+def getCoherenceFunctionXY_GPU_Method2(nSpatial,
+                                       nz,
+                                       nSample,
+                                       deltaZx,
+                                       deltaZy,
+                                       deltaZz,
+                                       weight,
+                                       eFieldReal,
+                                       eFieldImag, ):
+    """
+    We divide the reshaped time-averaged coherence function along the first dimension.
+
+    :param nSpatial1:  The length of the spatial index, which = nx * ny
+    :param nz:   The ends of the summation of the mutual coherence function
+    :param nSample:
+    :param deltaZx:
+    :param deltaZy:
+    :param deltaZz:
+    :param weight:
+    :param eFieldReal:
+    :param eFieldImag:
+    :return:
+    """
+    idx1, idx2 = cuda.grid(2)
+    contrast = cuda.device_array(1, np.float64)
+
+    if (idx1 < nSpatial) & (idx2 < nSpatial):
+        oldDeltaZ = 0
+        oldValue = 0.0
+
+        deltaZxy = deltaZx[idx2] + deltaZy[idx2] - (
+                deltaZx[idx1] + deltaZy[idx1])  # get the contribution from the xy direction in Q*(r2-r1)/k0
+
+        for sIdx in range(nSample):
+            deltaZ = int(deltaZxy + deltaZz(sIdx))  # Get the contribution from the z dimension
+
+            if abs(deltaZ) >= nz:
+                continue
+
+            # When deltaZ does not change, one does not need to calculate the time average again.
+            if deltaZ == oldDeltaZ:
+                # Because for the same z2 - z1, the summation is the same. One only needs to add a weight
+                # Add to the contrast
+                tmp = oldValue * weight[sIdx]
+                cuda.atomic.add(contrast, 0, tmp)
+                continue
+
+            # The delta Z determines the range over which time we calculate the average
+            zStart = max(0, -deltaZ)
+            zStop = min(nz, nz - deltaZ)
+
+            # Add the temporary holder
+            holderRealTmp = 0.0
+            holderImagTmp = 0.0
+
+            # Do the Time averaging
+            for tIdx in range(zStart, zStop):
+                holderRealTmp += eFieldReal[idx1, tIdx] * eFieldReal[idx2, tIdx + deltaZ]
+                holderRealTmp += eFieldImag[idx1, tIdx] * eFieldImag[idx2, tIdx + deltaZ]
+
+                holderImagTmp -= eFieldReal[idx1, tIdx] * eFieldImag[idx2, tIdx + deltaZ]
+                holderImagTmp += eFieldImag[idx1, tIdx] * eFieldReal[idx2, tIdx + deltaZ]
+
+            newValue = holderRealTmp ** 2 + holderImagTmp ** 2
+            contrast += newValue * weight[sIdx]
+
+            # Update the infor for the same deltaZ
+            oldDeltaZ = int(deltaZ)
+            oldValue = float(newValue)
+
+    return contrast
+
+
+#########################################################################################
+#            Yanwen's example code
 #########################################################################################
 def calc_gamma_t(nx, ny, nz, nsz, samx, samy, samz, Q_vec, eField, dt, w0):
     gamma_t = 0
